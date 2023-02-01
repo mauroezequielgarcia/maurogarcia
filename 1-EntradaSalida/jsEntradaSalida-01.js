6aa779fbb79c3e2bc2fa89cfa4ae2703f13b9aa7
mauroezequielgarcia <123269735+mauroezequielgarcia@users.noreply.github.com>
2023-01-31 22:03:45 -0300

    Add files via upload
    primer comit

---
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -0,0 +1,8 @@
+/*mauro 
+Al presionar el  botón, 
+se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
+function mostrar()
+{ let nombredepersona;
+	alert("esto funciona de maravilla");
+}
+
---
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -0,0 +1,9 @@
+/*mauro ezequiel garcia
+Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+
+function mostrar()
+{ let nombrepersona;
+	
+}nombrepersona = prompt("¿cual es el nombre?")
+alert("ok");
